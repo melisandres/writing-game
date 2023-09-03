c7cebafea086e4290e1786135a30a9232e2adbd8
melisandreschofield <melisandre.schofield@gmail.com>
2023-09-03 10:46:18 -0400

    keywords now appear on text-edit

--- text-update.php
+++ text-update.php
@@ -1,12 +1,20 @@
 <?php
-
-print_r($_POST); 
-
 require_once("class/Crud.php");
 $crud = new Crud;
-$update = $crud->update("text", $_POST);
 
-echo $update;
+
+
+
+
+
+
+
+
+
+
+
+
+$update = $crud->update("text", $_POST);
 
 
 
